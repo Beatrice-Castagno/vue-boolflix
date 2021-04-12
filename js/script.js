457b8803ffcee885714378d7e8d8229e9dd9928b
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-04-12 18:22:18 +0200

    Aggiunta ricerca serie tv + hover su card

--- js/script.js
+++ js/script.js
@@ -1,25 +1,49 @@
 var app = new Vue ({
  el: "#app",
  data: {
+ baseUrl: 'https://api.themoviedb.org/3/search/',
+ api_key: "feaf6db23feb7f3f96d8b38aa0e56ba0",
  searchInput: '',
- resultsSearch: [],
+ resultsMovie: [],
+ resultsTv: [],
+ indexMovie: null,
+ indexLanguage: 0,
+ language: [
+   'it-IT',
+   'en-EN',
+   'fr-FR',
+   'es-ES'
+ ]
  },
 
  methods: {
 
  searchFilm: function() {
- axios.get('https://api.themoviedb.org/3/search/movie', {
+ axios.get(this.baseUrl + 'movie', {
    params: {
-     api_key: "feaf6db23feb7f3f96d8b38aa0e56ba0",
+     api_key: this.api_key,
      query: this.searchInput,
-     language: "it-IT"
+     language: this.language[this.indexLanguage]
     }
    })
    .then( (risposta) => {
-    this.resultsSearch = risposta.data.results;
-    console.log(this.resultsSearch);
+    this.resultsMovie = risposta.data.results;
+    console.log(this.resultsMovie);
    });
-  }
+
+   axios.get(this.baseUrl + 'tv', {
+     params: {
+       api_key: this.api_key,
+       query: this.searchInput,
+       language: this.language[this.indexLanguage]
+      }
+     })
+     .then( (risposta) => {
+      this.resultsTv = risposta.data.results;
+      console.log(this.resultsTv);
+     });
+  },
+
  },
 
 });
