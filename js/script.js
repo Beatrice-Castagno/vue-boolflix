e59f334283e5cab28c823e9f37b8044f0540e1be
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-04-13 18:48:47 +0200

    Aggiunti stile, bandiere e funzionamenti Vuejs vari

--- js/script.js
+++ js/script.js
@@ -1,49 +1,45 @@
 var app = new Vue ({
  el: "#app",
  data: {
+ startSearch: false,
  baseUrl: 'https://api.themoviedb.org/3/search/',
  api_key: "feaf6db23feb7f3f96d8b38aa0e56ba0",
  searchInput: '',
  resultsMovie: [],
- resultsTv: [],
  indexMovie: null,
  indexLanguage: 0,
  language: [
-   'it-IT',
-   'en-EN',
-   'fr-FR',
-   'es-ES'
- ]
+   "it-IT",
+   "en-En",
+   "es-ES",
+   "de_DE"
+ ],
+ flags: [
+   'it',
+   'en',
+   'ja',
+   'es',
+   'de',
+  ]
  },
-
  methods: {
 
  searchFilm: function() {
- axios.get(this.baseUrl + 'movie', {
-   params: {
-     api_key: this.api_key,
-     query: this.searchInput,
-     language: this.language[this.indexLanguage]
+   if (this.searchInput != "")  {
+     axios.get(this.baseUrl + 'multi', {
+       params: {
+         api_key: this.api_key,
+         query: this.searchInput,
+         language: this.language[this.indexLanguage]
+        }
+       })
+       .then( (risposta) => {
+        this.resultsMovie = risposta.data.results;
+        console.log(this.resultsMovie);
+       });
+      this.startSearch = true;
     }
-   })
-   .then( (risposta) => {
-    this.resultsMovie = risposta.data.results;
-    console.log(this.resultsMovie);
-   });
-
-   axios.get(this.baseUrl + 'tv', {
-     params: {
-       api_key: this.api_key,
-       query: this.searchInput,
-       language: this.language[this.indexLanguage]
-      }
-     })
-     .then( (risposta) => {
-      this.resultsTv = risposta.data.results;
-      console.log(this.resultsTv);
-     });
-  },
-
+  }
  },
 
 });
